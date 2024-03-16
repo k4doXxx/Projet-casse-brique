1ab023dd070120169d848c524b2183280c77beb9
lecoyot <148537287+lecoyot@users.noreply.github.com>
2024-03-16 17:30:32 +0100

    Update CREATION BALLE.py

--- CREATION BALLE.py
+++ CREATION BALLE.py
@@ -1,1 +1,13 @@
+# ==== CREATION BALLE ====
 
+def balles_creation(x, y, balles_liste):
+    """création d'une balle avec la barre d'espace"""
+    
+    global nombre_balles
+
+    # btnr pour eviter les balles multiples
+    if pyxel.btnr(pyxel.KEY_SPACE):
+        if (nombre_balles < MAX_BALLES):
+            balles_liste.append([x+4, y-4])
+            nombre_balles += 1
+    return balles_liste
